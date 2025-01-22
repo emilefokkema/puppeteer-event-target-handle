ff146784ac6a075c6acf5836dd79694a62406495
Emile Fokkema <emilefokkema@gmail.com>
2025-01-22 20:46:48 +0100

    add script to build the types

--- src/local/api/index.ts
+++ src/local/api/index.ts
@@ -4,3 +4,7 @@ import { EventTargetHandleFactory } from './event-target-handle-factory';
 declare const createEventTargetHandleFactory: (page: Page) => Promise<EventTargetHandleFactory>;
 
 export { createEventTargetHandleFactory };
+export { EventTargetHandleFactory }
+export { EventTargetLike } from '../../shared/event-target-like'
+export { EventTargetHandle } from "./event-target-handle";
+export { ChartMap, SerializablePropertyChart, SerializedValue } from '../../shared/serializable-types'
